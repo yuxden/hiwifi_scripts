2f28eaa3ea76f2160bc9b4320d658daa952aa517
jm33-m0 <jm33.m0@gmail.com>
2016-07-16 19:47:18 +0800

    shadow.sh is now working

--- shadow.sh
+++ shadow.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#
+# Adapted by jm33_m0
 echo ''
-echo '看什么看'
+echo '===================================='
+echo 'Started installation'
+echo '===================================='
 echo ''
-sleep 5
+sleep 3 
 echo 'Creating temp directory... 创建临时目录ing...'
 echo ''
 mkdir /tmp/geewan
@@ -19,23 +21,18 @@ curl -k -o ss.tar.gz https://jm33.me/files/ss.tar.gz
 echo 'Done! 下载完成'
 echo '
 '
-sleep 3
+sleep 2
 echo 'Installing... 安装ing'
-cp ./shadowsocks.htm /usr/lib/lua/luci/view/admin_web/plugin/
-
-if test -e /usr/lib/lua/luci/view/admin_web/plugin/shadowsocks.htm;
-then echo 'Error'&&rm -rf /tmp/geewan&&exit;
-else
-if test -e /etc/config/ss-redir;
-then echo 'Error'&&rm -rf /tmp/geewan&&exit;
-else tar xzvf ss.tar.gz -C / >>/dev/null;
-fi
-fi
+tar xzvf ss.tar.gz;
+cp ../shadowsocks.htm /usr/lib/lua/luci/view/admin_web/network/
+cp ../index.htm /usr/lib/lua/luci/view/admin_web/network/
+cp ../style.css /www/turbo-static/turbo/web/css/
+cp ../ss-* /usr/bin/
 echo 'Done! 插件安装成功!'
 echo '
 '
-sleep 3
-echo 'Cleaning up... 准备删除临时文件...'
+sleep 2
+echo 'Writing uninstall info... 写入卸载信息ing'
 echo '
 '
 echo '
@@ -52,16 +49,12 @@ echo '
 echo 'Cleanning temporary files... 清理临时文件ing'
 echo '
 '
-if test -e /tmp/luci-indexcache;
-then rm /tmp/luci-indexcache&&echo 'Done! 清理完成 '&&echo '
+rm -rf /tmp/geewan
+if test -e /var/run/luci-indexcache;
+then rm /var/run/luci-indexcache&&echo 'Done! 清理完成 '&&echo '
 ';
 else echo 'luci-cache does not exist! 无法找到luci-cache,请确定是否是极路由环境'&&echo '
 '
 fi
-rm -rf /tmp/geewan
-cp ./shadowsocks.htm /usr/lib/lua/luci/view/admin_web/network/
-cp ./index.htm /usr/lib/lua/luci/view/admin_web/network/
-cp ./style.css /www/turbo-static/turbo/web/css/
-cp ./ss-* /usr/bin/
-sleep 3
+sleep 1
 echo 'Done! 插件成功安装!'
