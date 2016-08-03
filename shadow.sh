64402b9b99ad8edbec0978fc07b0f0b447250eac
jm33-m0 <jm33.m0@gmail.com>
2016-08-03 21:00:06 +0800

    added indicators

--- shadow.sh
+++ shadow.sh
@@ -7,36 +7,38 @@ echo '..........jm33_m0'
 echo '===================================='
 echo ''
 sleep 1 
-echo 'Creating temp directory... 创建临时目录ing...'
+echo '[*] Creating temp directory... 创建临时目录ing...'
 echo ''
 mkdir /tmp/geewan
 cd /tmp/geewan
-echo 'Done! 成功创建临时目录!'
+echo '[+] Done! 成功创建临时目录!'
 echo '
 '
 sleep 1
-echo 'Downloading files... 下载插件ing'
+echo '[*] Downloading files... 下载插件ing'
 echo '
 '
 curl -k -o ss.tar.gz https://jm33.me/files/ss.tar.gz
-echo 'Done! 下载完成'
+echo '[+] Done! 下载完成'
 echo '
 '
 sleep 1
 echo '#############################################################'
-echo 'Installing... 安装ing'
-echo ''
-echo '-------------------------------------------------------------'
-echo 'Unpacking... 释放安装文件ing'
+echo '
+[*] Installing... 安装ing
+'
+echo '-------------------------------------------------------------
+'
+echo '[*] Unpacking... 释放安装文件ing'
 tar xzvf ss.tar.gz -C /;
 echo '-------------------------------------------------------------'
 echo ''
-echo 'Done! 插件安装成功!'
+echo '[+] Done! 插件安装成功!'
 echo '#############################################################'
 sleep 1
 echo ''
 echo ''
-echo 'Writing uninstall info... 写入卸载信息ing'
+echo '[*] Writing uninstall info... 写入卸载信息ing'
 echo ''
 echo '
 ' >>/usr/lib/opkg/status
@@ -49,16 +51,16 @@ echo 'Installed-Time: 1422509506' >>/usr/lib/opkg/status
 echo 'Auto-Installed: yes' >>/usr/lib/opkg/status
 echo '
 ' >>/usr/lib/opkg/status
-echo 'Cleanning temporary files... 清理临时文件ing'
+echo '[*] Cleanning temporary files... 清理临时文件ing'
 echo '
 '
 rm -rf /tmp/geewan
 if test -e /var/run/luci-indexcache;
-then rm /var/run/luci-indexcache&&echo 'Done! 清理完成 '&&echo '
+then rm /var/run/luci-indexcache&&echo '[+] Done! 清理完成 '&&echo '
 ';
-else echo 'luci-cache does not exist! 无法找到luci-cache,请确定是否是极路由环境'&&echo '
+else echo '[-] luci-cache does not exist! 无法找到luci-cache,请确定是否是极路由环境'&&echo '
 '
 fi
 sleep 1
-echo 'Done! 插件成功安装!'
+echo '[+] Done! 插件成功安装!'
 echo ''
