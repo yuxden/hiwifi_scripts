8fd1852e193b31c722e703fa858500671273f4d5
jm33-m0 <jm33.m0@gmail.com>
2016-08-06 17:23:04 +0800

    debug info

--- hosts.sh
+++ hosts.sh
@@ -3,15 +3,16 @@
 echo '
 [*] Usage: '$0 '<RET>
 [!] 如果hosts未生效，请在路由器插件管理页面手动使用自定义hosts插件提交修改
+[*] 请查看/var/log/hosts.log中的debug信息
 '
 
 # tests if a cron job has been created already
 grep "hosts" /etc/crontabs/root > /dev/null
 if [ $? -eq 1 ]; then
-    echo `date`": [-] cron" >> /var/log/hosts.log
+    echo `date`": [-] cron job not found" >> /var/log/hosts.log
     echo "*/30 * * * sh /etc/hosts.sh" >> /etc/crontabs/root # check for update every 30 min
 else
-    echo `date`": [+] cron" >> /var/log/hosts.log
+    echo `date`": [+] cron job okay" >> /var/log/hosts.log
 fi
 
 # fetch hosts file from github
@@ -20,9 +21,9 @@ curl -k -o /tmp/hosts "https://raw.githubusercontent.com/racaljk/hosts/master/ho
 # append hosts record to a recognized file (if there is an update available)
 grep $(date +%Y-%m-%d) /tmp/hosts > /dev/null
 if [ $? -eq 1 ]; then
-    echo `date`": [-] update" >> /var/log/hosts.log # no update
+    echo `date`": [-] update not found" >> /var/log/hosts.log # no update
 else
-    echo `date`": [+] update" >> /var/log/hosts.log # update available, check if we have it installed already
+    echo `date`": [+] update available" >> /var/log/hosts.log # update available, check if we have it installed already
     grep $(date +%Y-%m-%d) /etc/hosts.d/openapi > /dev/null
     if  [ $? -eq 1 ]; then
         echo `date`": [+] updating..." >> /var/log/hosts.log
@@ -35,14 +36,14 @@ fi
 # auto start
 if ! test -e /etc/$0; then
     cp $0 /etc/
-    echo `date`": [-] etc" >> /var/log/hosts.log
+    echo `date`": [-] /etc/hosts.sh not found" >> /var/log/hosts.log
 else
-    echo `date`": [+] etc" >> /var/log/hosts.log
+    echo `date`": [+] /etc/hosts.sh okay" >> /var/log/hosts.log
 fi
 grep "hosts" /etc/rc.local > /dev/null
 if [ $? -eq 1  ]; then
-    echo `date`": [-] Auto start" >> /var/log/hosts.log
+    echo `date`": [-] Auto start not found" >> /var/log/hosts.log
     echo "sh /etc/"$0 > /etc/rc.local && echo "exit 0" >> /etc/rc.local
 else
-    echo `date`": [+] Auto start" >> /var/log/hosts.log
+    echo `date`": [+] Auto start okay" >> /var/log/hosts.log
 fi
